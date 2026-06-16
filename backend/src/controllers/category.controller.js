4bc924988fccd23a02df4ae86e10e733c1843502
mihirprabhakar <mihirprabhakar6@gmail.com>
2026-06-16 16:35:20 +0530

    added messages for all the endpoints defined in category.routes and define in category.controller

---
+++ backend/src/controllers/category.controller.js
@@ -0,0 +1,27 @@
+exports.getAllCategories=(req,res)=>{
+    res.json({
+        success:true,
+        message:"All category route working successfully"
+    });
+};
+
+exports.createCategory=(req,res)=>{
+    res.json({
+        success:true,
+        message:"Create category route working successfully"
+    });
+};
+
+exports.updateCategory=(req,res)=>{
+    res.json({
+        success:true,
+        message:"Update category route working successfully"
+    });
+};
+
+exports.deleteCategory=(req,res)=>{
+    res.json({
+        success:true,
+        message:"Delete category route working successfully"
+    });
+};
