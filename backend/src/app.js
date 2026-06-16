82244bbbc49cf721dd790459d9f84b722e6df27d
mihirprabhakar <mihirprabhakar6@gmail.com>
2026-06-16 17:36:25 +0530

    successfully imported the index file in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const productRoutes = require("./routes/product.routes");
+const routes = require("./routes/index");
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/products", productRoutes);
+
+app.get("/", (req, res) => {
+  res.json({
+     success: true,
+     message: "E-Commerce API is running" });
+});
 
 // check health
 app.get("/health", (req, res) => {
   res.json({ success: true, message: "Server is running correctly" });
 });
+
+app.use("/api", routes);
 
 module.exports = app;
