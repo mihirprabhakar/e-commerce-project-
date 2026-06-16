a4808c3799a63279121f6d651fdeb132030c1002
mihirprabhakar <mihirprabhakar6@gmail.com>
2026-06-16 17:34:45 +0530

    updated the index.js of src

---
+++ backend/src/routes/index.js
@@ -0,0 +1,19 @@
+const express=require("express");
+const router=express.Router();
+const authRoutes=require("./auth.routes");
+const productRoutes=require("./product.routes");
+const userRoutes=require("./user.routes");
+const dashboardRoutes=require("./dashboard.routes");
+const categoryRoutes=require("./category.routes");
+const cartRoutes=require("./cart.routes");
+const orderRoutes=require("./order.routes");
+
+
+router.use("/auth",authRoutes);
+router.use("/products",productRoutes)
+router.use("/users",userRoutes);
+router.use("/dashboard",dashboardRoutes);
+router.use("/categories",categoryRoutes);
+router.use("/cart",cartRoutes);
+router.use("/orders",orderRoutes);
+module.exports=router;
