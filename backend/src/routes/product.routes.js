428f302cf536c1b102e74d6a02c105ee0913c3e5
mihirprabhakar <mihirprabhakar6@gmail.com>
2026-06-16 17:07:15 +0530

    added all the routes endpoints in product.routes.js

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,8 +1,24 @@
 const express = require("express");
-const { bulkInsertProducts } = require("../controllers/product.controller");
+const productController = require("../controllers/product.controller");
 
 const router = express.Router();
 
-router.post("/bulk-insert",bulkInsertProducts);
+// @GET all products
+router.get("/",productController.getAllProducts);
+
+// @GET product by id
+router.get("/:id",productController.getProductById);
+
+// @POST create product 
+router.post("/",productController.createProduct);
+
+// @PUT update product 
+router.put("/:id",productController.updateProduct);
+
+// @Delete delete the product
+router.delete("/:id",productController.deleteProduct);
+
+// @post bulk insert products
+router.post("/bulk-insert",productController.bulkInsertProducts);
 
 module.exports = router;
